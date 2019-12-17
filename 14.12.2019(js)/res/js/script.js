1cfed199245c5e4c9dc21c817eef5acd8704aafb
Kertnik <Kertnik@users.noreply.github.com>
2019-12-17 17:37:57 +0200

    fdfd

---
+++ 14.12.2019(js)/res/js/script.js
@@ -0,0 +1,10 @@
+let a = parseInt(10 / 3);
+let b = Math.round(10 / 3, 0);
+let c = 3 ** 9;//3^9
+let str = 123;
+let sum_str = `${str} число`;
+let a = '10';
+let b = 10;
+let res1 = a == b;//true
+let res2 = a === b;//false
+a > 0 && a > b;//Только ДВА ЗНАКА
