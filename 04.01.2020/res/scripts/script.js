5206d9a352fb7ce5646674df5e8228fcc32f6398
Kertnik <nikita.voloshin04@gmail.com>
2020-01-04 21:49:01 +0200

    Start

---
+++ 04.01.2020/res/scripts/script.js
@@ -0,0 +1,47 @@
+//Strings methods
+/*
+let str = "Hi world!";
+console.log(str.charAt(5));
+console.log(str.indexOf("wor"));
+console.log(str.lastIndexOf("or"));
+let a = [str.split('')];
+console.log(a);
+console.log(str.substr(1, 5));//i wor
+console.log(str.substring(1, 5));// i wo
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());*/
+
+//pop push
+
+let arr = [];
+for (let i = 0; i < 10; i++) {
+
+    arr[i] = Math.round(Math.random() * 100);
+}
+
+console.log(arr.pop());
+arr.push(6)
+console.log(arr);
+console.log(arr.shift());
+arr.unshift(2);
+console.log(arr);
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(matrix[1][1] + " " + matrix[1][2]);
+
+console.log(arr);
+console.log("Сортировка " + arr.sort());
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+        if (arr[j] < arr[i]) {
+            let a = arr[j];
+            arr[j] = arr[i];
+            arr[i] = a;
+        }
+    }
+}
+console.log("Пузырчатая сортировка " + arr);
+function printMsg(from, text = "undefined text") {
+    alert(from + " say:" + text);
+}
+printMsg("Serg");
+printMsg("Serg", "Hello")
---
+++ DZ_for_08.01.2020/script.js
@@ -0,0 +1,44 @@
+let slova = ["воздух", "ребята", "солнце", "память", "вопрос", "тишина", "оружие", "густой", "точный", "мастер"];
+alert("Угадайте мое секретное слово!\n Оно содержит 6 букв,и вы можете угадывать одну букву за раз.Вам предоставляется 6 неправильных угадываний.");
+while (confirm("Сыграем в игру?")) {
+    let exceptions = [];
+    let lucky = 0;
+    let slovo = slova[Math.round(Math.random() * 10)].toUpperCase();
+    let vivod = [];
+    for (let i = 0; i < slovo.length; i++) {
+        vivod[i] = "_";
+    }
+    while (exceptions.length < 6 && lucky < slovo.length) {
+        let char = prompt("Угадай букву").toUpperCase();
+        if (char.length > 1) {
+            alert("Неверный ввод")
+        }
+        else {
+            if (slovo.includes(char)) {
+                for (let i = 0; i < slovo.length; i++) {
+                    if (slovo[i] == char) {
+                        lucky++;
+                        vivod[i] = char;
+                    }
+                }
+                alert("Угадал");
+            }
+            else {
+                alert("А вот и не угадал");
+                exceptions[exceptions.length] = " " + char;
+                alert(`Осталось ${6 - exceptions.length} попыток`);
+            }
+        }
+        alert(`${vivod.join(" ")}\n Неправильные варианты :${exceptions.join()}`);
+        char = "";
+    }
+    if (vivod.includes("_")) {
+        alert("Вы проиграли");
+    }
+    else {
+        alert(`Вы угадали, ваше слово:\"${slovo}\"`);
+    }
+
+}
+window.close();
+
